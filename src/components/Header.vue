7a3b6a7cb23f859b41eff7e9dff041ff290b171e
MattiaR <rovitomattia91@gmail.com>
2021-07-08 14:43:02 +0200

    add struttura header+main

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logo">
+      <!-- <i class="fab fa-spotify"></i> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.logo
+{
+    background-color: #2e3a46;
+    height: 80px;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bg-spotify">
+      <div class="container">
+          <div class="row">
+              <!-- <div class="col-2" v-for="song in songs"> -->
+
+              <!-- </div> -->
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Main',
+    // components : {
+    //     // MainList
+    // },
+    data(){
+        return {
+            apiURL : 'https://flynn.boolean.careers/exercises/api/array/music',
+            songs : ''
+        }
+    },
+    created(){
+        this.getSongs();
+    },
+    methods : {
+        getSongs(){
+            axios
+                .get(this.apiURL)
+                .then(res =>{
+                    console.log(res.data);
+                    this.songs= res.data;
+                    console.log(this.songs);
+                })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.bg-spotify
+{
+    background-color: #1e2d3b;
+    height: calc(100vh - 80px); 
+    
+}
+
+
+
+</style>
